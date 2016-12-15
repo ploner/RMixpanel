3860391c54838c2b7eaf2fe4309b656c1484e190
Ploner, Meinhard <meinhard.ploner@bcgdv.com>
2016-12-15 12:20:50 +0100

    Allow custom events in funnels and map event names optionally.

--- R/mixpanelGetFunnel.R
+++ R/mixpanelGetFunnel.R
@@ -1,14 +1,17 @@
 mixpanelGetFunnel <- function(
   account,
-  funnel,                 # Name <or> ID of the funnel.
+  funnel,             # Name <or> ID of the funnel.
   from,
   to=from,
-  verbose=TRUE,        # Level of verbosity.
-                          # the first step in the funnel. May not be greater than 90 days.
-  ...                     # Additional arguments to Mixpanel API. E.g.
-                          # >> interval=5
-                          # >> unit="week"
-                          # >> len=90
+  customEvents,       # If the funnel includes custom events, add a data.frame to assign 
+                      # readable names using the format: 
+                      #   data.frame(custom_event_id=c(121212, ...), event_name=c("Event One", ...))
+  verbose=TRUE,       # Level of verbosity.
+  ...                 # Additional arguments to Mixpanel API. E.g.
+                      # >> interval=5
+                      # >> unit="week"
+                      # >> len=90
+                      # the first step in the funnel. May not be greater than 90 days.
 ) {
   ## Extract funnel ID when <funnel> is name instead of ID.
   funnelList <- mixpanelGetFunnelList(account)
@@ -26,6 +29,22 @@ mixpanelGetFunnel <- function(
   data = jsonlite::fromJSON(data)
   res <- lapply(data$data, "[[", "steps")
   class(res) <- c("funnel", "list")
+  
+  if(!missing("customEvents")) {
+    ## Update all funnels.
+    for(iFunnel in 1:length(res)) {
+      funnel <- res[[iFunnel]]
+      
+      ## Replace ID with name.
+      isCustom <- !is.na(funnel$custom_event)
+      inds <- match(funnel$custom_event_id[isCustom], customEvents$custom_event_id)
+      funnel$event[isCustom] <- funnel$goal[isCustom] <- customEvents$event_name[inds] 
+      
+      res[[iFunnel]] <- funnel
+    }
+    
+  }
+  
   res
 }
 
