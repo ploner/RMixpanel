c87c5dfa9f82b4ee00d25367feedc28274f5a67e
Ploner, Meinhard <meinhard.ploner@bcgdv.com>
2017-01-16 10:32:28 +0100

    Minor changes in mixpanelGetEventsFromFiles

--- R/mixpanelGetEventsFromFiles.R
+++ R/mixpanelGetEventsFromFiles.R
@@ -1,30 +1,33 @@
 mixpanelGetEventsFromFiles = function(
   account,
-  dateFrom,
-  dateTo=dateFrom,
+  from,
+  to=from,
   eventNames=c(),       # Import only selected events?
   select=TRUE,          # Requested column names.
   blocksize=500000,     # Block size in reading files from disk. 
   verbose=TRUE
 ) {
-  dates = as.character(seq(as.Date(dateFrom), as.Date(dateTo), "day"))
+  dates = createDateSequence(from, to)
   alldata = matrix(NA, 0, 0)
   
   for (date in dates) {
-    cat("#########", date, "########", date(), "\n")
+    if(verbose)
+      cat("#########", date, "########", date(), "\n")
     
-    txtFilePath = paste(account$dataPath, "/", "Events-", date, ".txt", sep="")
+    txtFilePath = paste(account$dataPath, "/", "events-", date, ".txt", sep="")
     con <- file(txtFilePath, "r", blocking=TRUE)
     
     block = 1
     while (TRUE) {
       
-      cat("### Scan block", block, "of events  -", date(), "\n")
+      if(verbose)
+        cat("### Scan block", block, "of events  -", date(), "\n")
       data = readLines(con, n=blocksize)
       if(length(data) == 0)
         break
       
-      cat("### Subset, parse & merge   -", date(), "\n")
+      if(verbose)
+        cat("### Subset, parse & merge   -", date(), "\n")
       if (length(eventNames) > 0) {
         inds = rep(FALSE, length(data))
         for (name in eventNames) 
@@ -47,8 +50,10 @@ mixpanelGetEventsFromFiles = function(
     close(con)
   }
   
-  cat("### Flatten matrix           -", date(), "\n")
+  if(verbose)
+    cat("### Flatten matrix           -", date(), "\n")
   alldata = getFlatMatrix(alldata)
-  cat("### Done.                    -", date(), "\n")
+  if(verbose)
+    cat("### Done.                    -", date(), "\n")
   alldata
 }
